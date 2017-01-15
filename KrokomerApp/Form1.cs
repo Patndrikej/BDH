642904425f9abb9b9d56a6c2cb26f32e0bf0631f
Patndrikej <patndrikej@centrum.sk>
2017-01-15 13:58:32 +0100

    Pridaná C# aplikácia

---
+++ KrokomerApp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace KrokomerApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            int a;
+            InitializeComponent();
+
+            SerialPort port = connectPort();
+            string bb = port.ReadExisting();
+        }
+
+        private static SerialPort connectPort(){
+            string[] a = SerialPort.GetPortNames();
+            SerialPort mySerialPort = new SerialPort(a.First());
+
+            mySerialPort.BaudRate = 9600;
+            mySerialPort.Parity = Parity.None;
+            mySerialPort.StopBits = StopBits.One;
+            mySerialPort.DataBits = 8;
+            mySerialPort.Handshake = Handshake.None;
+            mySerialPort.Open();
+            
+            return mySerialPort;
+        }
+    }    
+}
