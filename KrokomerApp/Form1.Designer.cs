c6293594c31a63733512a89b5fedcd70ab3e6259
Patndrikej <patndrikej@centrum.sk>
2017-01-16 11:20:13 +0100

    Finalizácia aplikácie - Dokumentácia
    + Dokumentácia

--- KrokomerApp/Form1.Designer.cs
+++ KrokomerApp/Form1.Designer.cs
@@ -28,40 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(128, 102);
+            this.label1.Location = new System.Drawing.Point(279, 139);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 38);
+            this.label1.Size = new System.Drawing.Size(94, 38);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Time:";
+            this.label1.Text = "Time :";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(240, 109);
+            this.textBox1.Location = new System.Drawing.Point(374, 151);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(449, 26);
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(315, 26);
             this.textBox1.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(128, 179);
+            this.label2.Location = new System.Drawing.Point(128, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(245, 38);
             this.label2.TabIndex = 2;
@@ -69,26 +71,66 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(510, 191);
+            this.textBox2.Location = new System.Drawing.Point(374, 87);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(179, 26);
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(315, 26);
             this.textBox2.TabIndex = 3;
             // 
-            // chart1
+            // textBox3
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(135, 280);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(677, 300);
-            this.chart1.TabIndex = 4;
-            this.chart1.Text = "chart1";
+            this.textBox3.Location = new System.Drawing.Point(198, 594);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(315, 26);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "0,8";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(20, 582);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(177, 38);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Step length :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(201, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(172, 38);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Avg. speed :";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(374, 216);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(315, 26);
+            this.textBox4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(233, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 38);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Distance :";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(374, 276);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(315, 26);
+            this.textBox5.TabIndex = 8;
             // 
             // Form1
             // 
@@ -96,15 +138,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Highlight;
             this.ClientSize = new System.Drawing.Size(943, 642);
-            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.Text = "VRS - pedometer";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,7 +162,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
     }
 }
 
--- KrokomerApp/Form1.cs
+++ KrokomerApp/Form1.cs
@@ -13,13 +13,41 @@ namespace KrokomerApp
 {
     public partial class Form1 : Form
     {
+        string kroky;
+
+        System.Windows.Forms.Timer tmr = null;
+        TimeSpan _elapsed = new TimeSpan();
+        SerialPort port = connectPort();
         public Form1()
         {
-            int a;
             InitializeComponent();
+            StartTimer();
+            
+        }
+        void tmr_Tick(object sender, EventArgs e)
+        {
+            
+            _elapsed = _elapsed.Add(TimeSpan.FromMinutes(1));
+            textBox1.Text = _elapsed.ToString();
 
-            SerialPort port = connectPort();
+            //citanie z portu a uprava
             string bb = port.ReadExisting();
+            bb = bb.Replace("\r", ";");
+            string[] array = bb.Split(';');
+            string value = array[array.Length - 2];
+
+            if (Convert.ToInt32(value) >= Convert.ToInt32(kroky))
+            {
+                // vykonal sa krok
+                getValue(value);
+                kroky = value;
+
+            }
+            else
+            {
+                Application.Restart();
+            }
+
         }
 
         private static SerialPort connectPort(){
@@ -31,9 +59,67 @@ namespace KrokomerApp
             mySerialPort.StopBits = StopBits.One;
             mySerialPort.DataBits = 8;
             mySerialPort.Handshake = Handshake.None;
+           
             mySerialPort.Open();
-            
+
             return mySerialPort;
         }
-    }    
+
+        private void getValue(string stepCount)
+        {
+            double distance = (Convert.ToInt32(stepCount) * Convert.ToDouble(textBox3.Text));
+            double speed = (distance/Convert.ToDouble(_elapsed.Minutes))*3.6;
+            textBox2.Text = stepCount;                    //number of steps output
+            textBox2.ForeColor = Color.Red;
+            textBox4.Text = speed.ToString() + " km/h";             //avg. speed
+            textBox5.Text = distance.ToString() + " m";   //distance outut
+
+        }
+
+        private void StartTimer()
+        {
+            tmr = new System.Windows.Forms.Timer();
+            tmr.Interval = 1000;
+            tmr.Tick += new EventHandler(tmr_Tick);
+            tmr.Enabled = true;
+        }
+    }
+    /*private void parse(string input){
+            /*
+            double value;
+            string temp = input.Replace(";", "");
+            string[] subStrings = temp.Split(':');
+
+            kroky = int.Parse(subStrings[1]);
+
+            value = double.Parse(subStrings[3]);
+            hodnota1 = addElement(hodnota1, value);
+
+            value = double.Parse(subStrings[5]);
+            trashhold = addElement(trashhold, value);
+
+            value = double.Parse(subStrings[7]);
+            min = addElement(min, value);
+
+            value = double.Parse(subStrings[9]);
+            max = addElement(max, value);
+
+            reset = Boolean.Parse(subStrings[11]);
+            
+        }
+
+        private double[] addElement(double[] array,double value)
+        {
+            int pos = array.Length - 1;
+
+            if (array[pos] != 0) {
+                Array.Resize(ref array, array.Length + 1);
+                array[pos] = value;
+            } else {
+                array[pos] = value;
+            }
+
+            return array;
+        }
+        */
 }
